50f341d49b267bdbf25f0a5437532259e07f557f
Andy Davis <AndrewMarkham@seznam.cz>
2019-09-27 11:05:52 +0200

    edits

--- js-playgrounds-1/first-login/src/js/index.js
+++ js-playgrounds-1/first-login/src/js/index.js
@@ -1,1 +1,18 @@
-console.log('It works');
+/* eslint-disable */
+const users = [
+  ['John', 12345],
+  ['Jane', 11111],
+  ['Allen', 54321],
+  ['Andrea', 80808],
+  ['Lacie', 55555]];
+
+const checkUser = () => { 
+  const getUserName = document.querySelector('#user-name');
+  const userName = getUserName.value;
+  for (let i = 0; i <= users.length; i++) {
+    if (users[i] = ) {
+        console.log('It works!')
+    }
+  }
+ };
+
